e815379d88b90206fffc933b2326fa7a4fd83030
khacngocptit <khacngocptit@gmail.com>
2021-05-17 09:06:05 +0700

    'up'

--- app/views/eshop/signup.php
+++ app/views/eshop/signup.php
@@ -1,22 +1,124 @@
 <?php $this->view("eshop/layout/header");?>
 
-    <section id="form"><!--form-->
+    <div class='wrapper bg-wrapper'>
         <div class="container">
             <div class="row">
-                <div class="col-md-6 offset-md-3 col-8 offset-2">
-                    <div class="signup-form"><!--sign up form-->
-                        <h2>New User Signup!</h2>
-                        <form action="#">
-                            <input type="text" placeholder="Name"/>
-                            <input type="email" placeholder="Email Address"/>
-                            <input type="password" placeholder="Password"/>
-                            <button type="submit" class="btn btn-default">Signup</button>
+                <div class="col-md-12">
+                    <div class="form-signup mt-5">
+                        <form class="form-horizontal" role="form" id="signup-form" action="#" method="POST">
+                            <h2 class="form__name">Sign up</h2>
+                            <div class="row">
+                                <div class="col-lg-8 col-xl-6 offset-xl-3 offset-lg-2">
+                                    <div class="form-group mb-4 row">
+                                        <label for="firstName" class="col-md-3 control-label  d-flex">First
+                                            Name</label>
+                                        <div class="col-md-9">
+                                            <input type="text" id="firstName" name="firstname" placeholder="First Name" class="form-control" autofocus>
+                                        </div>
+                                    </div>
+                                    <div class="form-group  mb-4  row">
+                                        <label for="lastName" class="col-md-3 control-label  d-flex">Last
+                                            Name</label>
+                                        <div class="col-md-9">
+                                            <input type="text" id="lastName" name="lastname" placeholder="Last Name" class="form-control" autofocus>
+                                        </div>
+                                    </div>
+                                    <div class="form-group  mb-4 row">
+                                        <label for="email" class="col-md-3 control-label  d-flex ">Email
+                                        </label>
+                                        <div class="col-md-9">
+                                            <input type="email" id="email" placeholder="Email" class="form-control" name="email">
+                                        </div>
+                                    </div>
+                                    <div class="form-group  mb-4 row">
+                                        <label for="phoneNumber" class="col-md-3 control-label  d-flex ">Phone
+                                            number
+                                        </label>
+                                        <div class="col-md-9">
+                                            <input type="phoneNumber" name="phonenumber" id="phoneNumber" placeholder="Phone number" class="form-control">
+                                        </div>
+                                    </div>
+                                    <div class="form-group  mb-4 row">
+                                        <label for="dateofbirth" class="col-md-3 control-label  d-flex ">Date of
+                                            Birth</label>
+                                        <div class="col-md-9">
+                                            <input type="date" id="dateofbirth" name="dateofbirth" class="form-control">
+                                        </div>
+                                    </div>
+                                    <div class="form-group  mb-4 row">
+                                        <label class="control-label col-md-3 d-flex ">Gender</label>
+                                        <div class="col-md-9">
+                                            <div class="row">
+                                                <div class="col-sm-4">
+                                                    <label class="radio-inline">
+                                                        <input type="radio" id="femaleRadio" name="gender"
+                                                               value="Female">
+                                                        Male
+                                                    </label>
+                                                </div>
+                                                <div class="col-sm-4">
+                                                    <label class="radio-inline">
+                                                        <input type="radio" id="maleRadio" value="Male"
+                                                               name="gender"> Female
+                                                    </label>
+                                                </div>
+                                                <div class="col-sm-4">
+                                                    <label class="radio-inline">
+                                                        <input type="radio" id="maleRadio" value="Other"
+                                                               name="gender"> Other
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group  mb-4 row">
+                                        <label for="password" class="col-md-3 control-label  d-flex ">Nationality</label>
+                                        <div class="col-md-9">
+                                            <input type="text" id="nationality" name="nationality" placeholder="Nationality" class="form-control">
+                                        </div>
+                                    </div>
+                                    <div class="form-group  mb-4 row">
+                                        <label for="password" class="col-md-3 control-label  d-flex ">Password</label>
+                                        <div class="col-md-9">
+                                            <input type="password" id="password" name="password" placeholder="Password" class="form-control">
+                                        </div>
+                                    </div>
+                                    <div class="form-group mb-4 row">
+                                        <label for="password" class="col-md-3 control-label  d-flex ">Confirm
+                                            Password</label>
+                                        <div class="col-md-9">
+                                            <input type="password" id="confirmpassword" name="confirmpassword" placeholder="Confirm Password" class="form-control">
+                                        </div>
+                                    </div>
+                                    <hr />
+                                    <!-- /.form-group -->
+                                    <div class="form-group">
+                                        <div class="col-md-12">
+                                            <div class="checkbox">
+                                                <label>
+                                                    <input type="checkbox" value="isagree" id="isagree"
+                                                           name="isagree">&emsp;
+                                                    I accept the <a href="#">Terms of Service</a>, <a href="#">Privacy Policy</a> and <a href="#">User Of
+                                                        Personal Information</a>
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <button type="submit" class="btn_pink btn_custom w-100 btn_pc">Complete Sign
+                                        up</button>
+                                    <button type="submit" class="btn_pink btn_custom w-100 btn_sp">Complete
+                                        Registration</button>
+                                </div>
+                            </div>
+                            <p class="text-center mt-4">Already Registered? <a href="login.html">Log in</a> here</p>
                         </form>
-                    </div><!--/sign up form-->
+                        <!-- /form -->
+                    </div>
                 </div>
             </div>
         </div>
-    </section><!--/form-->
+    </div>
+
 
 
 <?php $this->view("eshop/layout/footer");?>
