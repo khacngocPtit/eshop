66cbbf890c609f7dde5f9b788df36546ff1f5665
khacngocptit <khacngocptit@gmail.com>
2021-05-28 10:33:21 +0700

    'Window;

--- quan_ly_san_pham.sql
+++ quan_ly_san_pham.sql
@@ -2,8 +2,8 @@
 -- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Máy chủ: localhost
--- Thời gian đã tạo: Th5 14, 2021 lúc 03:32 AM
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th5 28, 2021 lúc 05:32 AM
 -- Phiên bản máy phục vụ: 10.4.18-MariaDB
 -- Phiên bản PHP: 8.0.5
 
@@ -30,11 +30,11 @@ SET time_zone = "+00:00";
 CREATE TABLE `tbl_cart` (
   `id` int(30) NOT NULL,
   `pay` int(1) NOT NULL,
+  `total_money` int(50) NOT NULL,
   `product_id` int(30) NOT NULL,
   `user_id` int(30) NOT NULL,
-  `total_price` bigint(100) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `created_at` time NOT NULL,
+  `updated_at` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -47,8 +47,8 @@ CREATE TABLE `tbl_category` (
   `id` int(30) NOT NULL,
   `category_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `category_decs` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `modified_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `created_at` time NOT NULL,
+  `updated_at` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -59,16 +59,19 @@ CREATE TABLE `tbl_category` (
 
 CREATE TABLE `tbl_product` (
   `id` int(30) NOT NULL,
-  `product_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL,
-  `product_detail` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `product_price` bigint(20) NOT NULL,
-  `product_number` int(100) NOT NULL,
+  `product_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `product_decs` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `product_price` int(30) NOT NULL,
+  `product_number` int(30) NOT NULL,
   `product_promotion` float NOT NULL,
-  `product_url` varchar(1000) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image4` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `category_id` int(30) NOT NULL,
-  `product_status` tinyint(1) NOT NULL,
-  `create_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `update_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `product_status` int(1) NOT NULL,
+  `created_at` time NOT NULL,
+  `updated_at` time NOT NULL,
+  `image1` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `image2` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `image3` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -79,15 +82,26 @@ CREATE TABLE `tbl_product` (
 
 CREATE TABLE `tbl_user` (
   `id` int(30) NOT NULL,
-  `username` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `fullname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `gender` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phonenumber` varchar(11) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `role` int(1) NOT NULL,
-  `date_of_birth` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `create_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `update_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `date_of_birth` date NOT NULL,
+  `created_at` time NOT NULL,
+  `updated_at` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id`, `username`, `password`, `fullname`, `gender`, `phonenumber`, `email`, `role`, `date_of_birth`, `created_at`, `updated_at`) VALUES
+(4, 'testid01', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'Tran Van A', 'Female', '0123013013', 'khacngocptit@gmail.com', 1, '2021-04-28', '00:00:00', '00:00:00'),
+(5, 'ngocbk', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'Bui Khac B', 'Female', '0123013013', 'khacngocptit@gmail.com', 1, '0000-00-00', '00:00:00', '00:00:00'),
+(6, 'admin', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'admin', 'Female', '3212133123', 'admin@example.com', 2, '2021-04-28', '00:00:00', '00:00:00');
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -98,8 +112,8 @@ CREATE TABLE `tbl_user` (
 --
 ALTER TABLE `tbl_cart`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `product_id` (`product_id`),
-  ADD KEY `user_id` (`user_id`);
+  ADD KEY `tbl_cart_fk0` (`product_id`),
+  ADD KEY `tbl_cart_fk1` (`user_id`);
 
 --
 -- Chỉ mục cho bảng `tbl_category`
@@ -112,14 +126,13 @@ ALTER TABLE `tbl_category`
 --
 ALTER TABLE `tbl_product`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `category_id` (`category_id`);
+  ADD KEY `tbl_product_fk0` (`category_id`);
 
 --
 -- Chỉ mục cho bảng `tbl_user`
 --
 ALTER TABLE `tbl_user`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT cho các bảng đã đổ
@@ -147,7 +160,7 @@ ALTER TABLE `tbl_product`
 -- AUTO_INCREMENT cho bảng `tbl_user`
 --
 ALTER TABLE `tbl_user`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Các ràng buộc cho các bảng đã đổ
@@ -157,14 +170,14 @@ ALTER TABLE `tbl_user`
 -- Các ràng buộc cho bảng `tbl_cart`
 --
 ALTER TABLE `tbl_cart`
-  ADD CONSTRAINT `product_id` FOREIGN KEY (`product_id`) REFERENCES `tbl_product` (`id`),
-  ADD CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `tbl_user` (`id`);
+  ADD CONSTRAINT `tbl_cart_fk0` FOREIGN KEY (`product_id`) REFERENCES `tbl_product` (`id`),
+  ADD CONSTRAINT `tbl_cart_fk1` FOREIGN KEY (`user_id`) REFERENCES `tbl_user` (`id`);
 
 --
 -- Các ràng buộc cho bảng `tbl_product`
 --
 ALTER TABLE `tbl_product`
-  ADD CONSTRAINT `category_id` FOREIGN KEY (`category_id`) REFERENCES `tbl_category` (`id`);
+  ADD CONSTRAINT `tbl_product_fk0` FOREIGN KEY (`category_id`) REFERENCES `tbl_category` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
